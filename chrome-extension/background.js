03f6dd1b8c71b0b7e15305ed5d51494dcb41c64e
PoWen <powen84@gmail.com>
2016-05-22 08:50:27 +0800

    改變 httpPost 方式, 新增 log textarea, 提取登入指令

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -5,8 +5,8 @@ msgToPopup[0] = {};
 // tabId, 
 // msgToPopup.forEach(function(tabMsg) {if(tabMsg.tabId == xxx) {console.log('hi',tabMsg.tabId)}})
 
-var serverInfo;
-var tabSwitcher = 0;
+var serverInfo; // the container to store response information from httpPostString method 
+var tabSwitcher = 0; // the state of the active tab
 
 var getCurrentTabId = function (callback) {
   var queryInfo = {
@@ -70,9 +70,14 @@ chrome.extension.onConnect.addListener(function(port) {
   });
 });
 
-var httpPostString = function(stringContent) {
+
+// Post String to URL
+// Two kinds of callback:
+// i) responseInfo: print the parsed JSON server response to console
+// ii) getResponseInfo: put the parsed JSON server response into global variable "serverInfo"
+var httpPostString = function(stringContent, url, callback) {
   var httpRequest = new XMLHttpRequest();
-  httpRequest.open("POST",msgToPopup[tabSwitcher].url,true);
+  httpRequest.open("POST",url,true);
   httpRequest.setRequestHeader("Accept","*/*");
   httpRequest.setRequestHeader("Accept-Language","zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4,zh-CN;q=0.2");
   httpRequest.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
@@ -81,9 +86,15 @@ var httpPostString = function(stringContent) {
 
   httpRequest.onreadystatechange=function() {
     if (httpRequest.readyState==4 && httpRequest.status==200) {
-        serverInfo = JSON.parse(httpRequest.responseText);
+        callback(httpRequest.responseText);
     }
   }
 }
 
+var responseInfo = function(responseText) {
+  console.log(JSON.parse(responseText));
+}
 
+var getResponseInfo = function(responseText) {
+  serverInfo = JSON.parse(responseText)
+}
--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -13,35 +13,11 @@ function getCurrentTabId(callback) {
   };
 
   chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
     var tab = tabs[0];
     tabObj = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
     var tabId = tab.id;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    //console.assert(typeof id == 'string', 'tab.url should be a string');
-
     callback(tabId);
   });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
 getCurrentTabId(function(id){
@@ -49,8 +25,13 @@ getCurrentTabId(function(id){
 })
 
 function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
+  document.getElementById('status').textContent = statusText + '\n' + document.getElementById('status').textContent;
 }
+
+function renderDiv(divName,divText) {
+  document.getElementById(divName).textContent = divText;
+}
+
 
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('button1').addEventListener('click', myFirstExtFunc);
@@ -73,7 +54,7 @@ var myFirstExtFunc = function() {
 var bossWarTimerCalledFlag = 0;
 var bossWar = function() {
   var today = new Date();
-  if (today.getDay() == 0 || today.getDay() == 5) {
+  if (today.getDay() == 0 || today.getDay() == 6) {
     renderStatus('今天有神將!');
     
     if (bossWarTimerCalledFlag == 0) {
@@ -83,21 +64,10 @@ var bossWar = function() {
         var d = new Date();
         var restSec = 20*3600 - d.getHours()*3600 - d.getMinutes()*60 - d.getSeconds();
         var renderString;
-        renderString = '神將倒數 ' + Math.floor(restSec/3600) + ':' 
+        divString = '神將倒數 ' + Math.floor(restSec/3600) + ':' 
                               + Math.floor((restSec%3600)/60) + ':' 
                               + Math.floor((restSec%3600)%60);
-        renderStatus(renderString); 
-        // console.log(d.toLocaleTimeString());
-        // if (d.getDay() == 0 & d.getHours() == 20 & d.getMinutes() >= 0 & d.getMinutes() <= 30 & d.getSeconds() >= 0) {
-        //   timeFlag = 1;
-        //   troopFlag+= 1;
-        // }
-        // else timeFlag = 0;
-
-        // if ((troopFlag%31) == 1) {
-        //   sendTroops();
-        //   console.log("送出隊伍!")
-        // }
+        renderDiv('boss-war-status',divString); 
       }
       bossWarTimerCalledFlag = 1;
     } else {
